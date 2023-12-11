09f2bdd78914cdaea530dd391f822f442190a945
gabriel <gabrielaugustosb13@gmail.com>
2023-12-11 12:25:04 -0300

    finaliza backtracking

--- src/main/java/org/example/BackTracking.java
+++ src/main/java/org/example/BackTracking.java
@@ -6,52 +6,84 @@ import java.util.HashSet;
 import java.util.List;
 
 public class BackTracking {
+
+    private static SubConjunto melhorDistribuicao = null;
+    private static double melhorMediaGlobal = 0;
+
     public static void gerarCombinacoes(int[] conjunto) {
-        SubConjunto melhorDistribuicao = null;
-        double melhorMediaGlobal = 0;
-
         int n = conjunto.length;
-        HashSet<SubConjunto> subConjuntosGlobais = new HashSet<>();
+        boolean[] usado = new boolean[n];
+        List<Integer> caminhao1 = new ArrayList<>();
+        List<Integer> caminhao2 = new ArrayList<>();
+        List<Integer> caminhao3 = new ArrayList<>();
 
-        // Gera todas as combinações possíveis de 3 subconjuntos
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (i != j && i != k && j != k) { // poda
-                        ArrayList<ArrayList<Integer>> subconjuntos = new ArrayList<>();
-                        subconjuntos.add(new ArrayList<>(Arrays.asList(conjunto[i], conjunto[j])));
-                        subconjuntos.add(new ArrayList<>(Arrays.asList(conjunto[k])));
-
-                        ArrayList<Integer> restantes = new ArrayList<>();
-                        for (int l = 0; l < n; l++) {
-                            if (l != i && l != j && l != k) {
-                                restantes.add(conjunto[l]);
-                            }
-                        }
-
-                        subconjuntos.add(new ArrayList<>(restantes));
-                        SubConjunto novoSubConjuntoCandidato = new SubConjunto(subconjuntos.get(0), subconjuntos.get(1), subconjuntos.get(2));
-                        subConjuntosGlobais.add(novoSubConjuntoCandidato);
-
-                        double mediaCaminhao1 = novoSubConjuntoCandidato.calcularPorcentagem(novoSubConjuntoCandidato.totalcaminhao1);
-                        double mediaCaminhao2 = novoSubConjuntoCandidato.calcularPorcentagem(novoSubConjuntoCandidato.totalcaminhao2);
-                        double mediaCaminhao3 = novoSubConjuntoCandidato.calcularPorcentagem(novoSubConjuntoCandidato.totalcaminhao3);
-                        double melhorMedialocal = obtermediaLocal(mediaCaminhao1, mediaCaminhao2, mediaCaminhao3);
-
-                        if(melhorMedialocal > melhorMediaGlobal ) {
-                            melhorMediaGlobal = melhorMedialocal;
-                            melhorDistribuicao = novoSubConjuntoCandidato;
-                        }
-                    }
-                }
-            }
-        }
+        backtrack(conjunto, usado, caminhao1, caminhao2, caminhao3);
 
         System.out.println("Melhor distribuicao: " + melhorDistribuicao);
     }
 
-    public static double obtermediaLocal(double a, double b, double c) {
-        // Se um dos números for 100, consideramos o próximo maior
+    private static void backtrack(int[] conjunto, boolean[] usado, List<Integer> caminhao1, List<Integer> caminhao2, List<Integer> caminhao3) {
+        if (caminhao1.size() + caminhao2.size() + caminhao3.size() == conjunto.length) {
+            // Calcular médias
+            double mediaCaminhao1 = calcularMedia(caminhao1);
+            double mediaCaminhao2 = calcularMedia(caminhao2);
+            double mediaCaminhao3 = calcularMedia(caminhao3);
+            double mediaGlobal = obtermediaLocal(mediaCaminhao1, mediaCaminhao2, mediaCaminhao3);
+
+            // Atualizar melhor distribuição
+            if (mediaGlobal > melhorMediaGlobal) {
+                melhorMediaGlobal = mediaGlobal;
+                melhorDistribuicao = new SubConjunto(new ArrayList<>(caminhao1), new ArrayList<>(caminhao2), new ArrayList<>(caminhao3));
+            }
+            return;
+        }
+
+        for (int i = 0; i < conjunto.length; i++) {
+            if (!usado[i]) {
+                usado[i] = true;
+
+                // Adicionar elemento ao caminhão com menor carga
+                int somaCaminhao1 = somarElementos(caminhao1);
+                int somaCaminhao2 = somarElementos(caminhao2);
+                int somaCaminhao3 = somarElementos(caminhao3);
+
+                if (somaCaminhao1 <= somaCaminhao2 && somaCaminhao1 <= somaCaminhao3) {
+                    caminhao1.add(conjunto[i]);
+                } else if (somaCaminhao2 <= somaCaminhao3) {
+                    caminhao2.add(conjunto[i]);
+                } else {
+                    caminhao3.add(conjunto[i]);
+                }
+
+                backtrack(conjunto, usado, caminhao1, caminhao2, caminhao3);
+
+                // Desfazer a escolha para explorar outras opções
+                if (somaCaminhao1 <= somaCaminhao2 && somaCaminhao1 <= somaCaminhao3) {
+                    caminhao1.remove(caminhao1.size() - 1);
+                } else if (somaCaminhao2 <= somaCaminhao3) {
+                    caminhao2.remove(caminhao2.size() - 1);
+                } else {
+                    caminhao3.remove(caminhao3.size() - 1);
+                }
+
+                usado[i] = false;
+            }
+        }
+    }
+
+    private static double calcularMedia(List<Integer> caminhao) {
+        if (caminhao.isEmpty()) {
+            return 0;
+        }
+
+        int soma = 0;
+        for (int valor : caminhao) {
+            soma += valor;
+        }
+        return (double) soma / caminhao.size();
+    }
+
+    private static double obtermediaLocal(double a, double b, double c) {
         if (a == 100) {
             return (c + b) / 2;
         } else if (b == 100) {
@@ -61,5 +93,9 @@ public class BackTracking {
         }
 
         return (a + b + c) / 3;
+    }
+
+    private static int somarElementos(List<Integer> lista) {
+        return lista.stream().mapToInt(Integer::intValue).sum();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,26 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //int[] conjunto1 = {1, 2, 3, 4, 5, 6};
+        // BACKTRACKING
+        /*int tamConjunto = 10;
+        List<int[]> rotasGeradas = GeradorDeProblemas.geracaoDeRotas(11, tamConjunto, 0.5);
 
-       // BackTracking backTracking = new BackTracking();
-       // backTracking.gerarCombinacoes(conjunto1);
-        List<int[]> rotas = GeradorDeProblemas.geracaoDeRotas(10, 5, 0.5);
+        double mediaDeExecucoes = 0;
+        for (int[] array : rotasGeradas) {
+            long startTime = System.currentTimeMillis();
 
-        // Chamada Divisao E Conquista
+            BackTracking backTracking = new BackTracking();
+            backTracking.gerarCombinacoes(array);
 
-        DistribuicaoRotasDivisaoConquista.distribuirRotas();
+            long endTime = System.currentTimeMillis();
+            long executionTime = endTime - startTime;
+
+            mediaDeExecucoes += executionTime;
+        }
+
+        mediaDeExecucoes = mediaDeExecucoes / tamConjunto;
+        System.out.println("Media de execuções em ms: " + mediaDeExecucoes);
+         */
     }
 }
 
