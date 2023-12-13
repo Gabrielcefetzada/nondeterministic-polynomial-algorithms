5974f67f8dbbe851d9fcc678c5065ddd1beda1d2
Rafael Pierre <pierredoc28@gmail.com>
2023-12-13 20:21:38 -0300

    fix: media

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,10 @@ public class Main {
         int numCaminhoes, numRotas, tamanhoConjunto, opcao;
         double dispersao;
         boolean continua = true;
+        long startTime = 0;
+        long endTime = 0;
+        long executionTime =0;
+        double mediaDeExecucoes = 0;
 
         System.out.println("Digite o número de caminhões");
         numCaminhoes = scanner.nextInt();
@@ -39,42 +43,66 @@ public class Main {
             System.out.println("6 - Sair");
             opcao = scanner.nextInt();
 
-            double mediaDeExecucoes = 0;
-            long startTime = System.currentTimeMillis();
             switch (opcao) {
                 case 1:
+                    startTime = System.currentTimeMillis();
 
                     for (int[] array : rotasGeradas) {
                         BackTracking backTracking = new BackTracking();
                         backTracking.gerarCombinacoes(array);
                     }
 
+                    endTime = System.currentTimeMillis();
+                    executionTime = endTime - startTime;
+                    mediaDeExecucoes += executionTime;
+
                     break;
                 case 2:
+                    startTime = System.currentTimeMillis();
 
                     for (int[] array : rotasGeradas) {
                         EstrategiaGulosa1 estrategiaGulosa1 = new EstrategiaGulosa1();
                         estrategiaGulosa1.distribuirRotas(array, numCaminhoes);
                     }
 
+                    endTime = System.currentTimeMillis();
+                    executionTime = endTime - startTime;
+                    mediaDeExecucoes += executionTime;
+
                     break;
                 case 3:
+                    startTime = System.currentTimeMillis();
 
                     for (int[] array : rotasGeradas) {
                         EstrategiaGulosa2 guloso2 = new EstrategiaGulosa2();
                         guloso2.distribuiRotasParaCaminhoes(numCaminhoes, array);
                     }
 
+                    endTime = System.currentTimeMillis();
+                    executionTime = endTime - startTime;
+                    mediaDeExecucoes += executionTime;
+
                     break;
                 case 4:
+                    startTime = System.currentTimeMillis();
 
                     for (int[] array : rotasGeradas) {
                         DistribuicaoRotasDivisaoConquista.distribuiRotas(array, numCaminhoes);
                     }
 
+                    endTime = System.currentTimeMillis();
+                    executionTime = endTime - startTime;
+                    mediaDeExecucoes += executionTime;
+
                     break;
                 case 5:
+                    startTime = System.currentTimeMillis();
+
                     System.out.println("Método ainda nao implementado");
+
+                    endTime = System.currentTimeMillis();
+                    executionTime = endTime - startTime;
+                    mediaDeExecucoes += executionTime;
                     break;
 
                 case 6:
@@ -86,12 +114,8 @@ public class Main {
                     System.out.println("Esta não é uma opção válida!");
             }
 
-            long endTime = System.currentTimeMillis();
-            long executionTime = endTime - startTime;
 
-            mediaDeExecucoes += executionTime;
-
-            System.out.println("Media de execuções em ms: " + mediaDeExecucoes);
+            System.out.println("Media de execuções em ms: " + mediaDeExecucoes/tamanhoConjunto);
         }
     }
 }
