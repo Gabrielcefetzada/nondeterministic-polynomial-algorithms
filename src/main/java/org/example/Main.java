bc1ec875cb177182956f37b9d742d6a09d9f5a2b
Rafael Pierre <pierredoc28@gmail.com>
2023-12-12 21:20:24 -0300

    rebuild: Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -85,46 +85,6 @@ public class Main {
                     System.out.println("Esta não é uma opção válida!");
             }
         }
-                // BACKTRACKING
-        /*
-         * int tamConjunto = 10;
-         * List<int[]> rotasGeradas = GeradorDeProblemas.geracaoDeRotas(11, tamConjunto,
-         * 0.5);
-         *
-         * double mediaDeExecucoes = 0;
-         * for (int[] array : rotasGeradas) {
-         * long startTime = System.currentTimeMillis();
-         *
-         * BackTracking backTracking = new BackTracking();
-         * backTracking.gerarCombinacoes(array);
-         *
-         * long endTime = System.currentTimeMillis();
-         * long executionTime = endTime - startTime;
-         *
-         * mediaDeExecucoes += executionTime;
-         * }
-         *
-         * mediaDeExecucoes = mediaDeExecucoes / tamConjunto;
-         * System.out.println("Media de execuções em ms: " + mediaDeExecucoes);
-         */
 
-        // DIVISAO E CONQUISTA
-        /*
-         * int NumeroVeiculos = 3;
-         * List<int[]> rotas = GeradorDeProblemas.geracaoDeRotas(10, 5, 0.5);
-         *
-         * // Chamada Divisao E Conquista
-         * for (int[] arrayRotas : rotas) {
-         * System.out.println("");
-         * DistribuicaoRotasDivisaoConquista.distribuirRotas(arrayRotas,
-         * NumeroVeiculos);
-         * }
-         *
-         */
-
-
-        // GULOSO 1
-
-        // GULOSO 2
     }
 }
